840b1302de5ca74a24ed60a9939909725a3ea75f
Richard Hanten <richard.hanten@uni-tuebingen.de>
2015-06-02 13:27:40 +0200

    fix

--- include/utils_vision/utils/cluster_boundaries.hpp
+++ include/utils_vision/utils/cluster_boundaries.hpp
@@ -11,12 +11,24 @@ void getClusterBoundaryMask(const cv::Mat& src,
     dst = cv::Mat::zeros(src.rows, src.cols, CV_8UC1);
     const LabelT *src_ptr = src.ptr<LabelT>();
     uchar *dst_ptr = dst.ptr<uchar>();
-    for(int i = 1 ; i < src.rows - 1 ; ++i) {
-        for(int j = 1 ; j < src.cols - 1; ++j) {
-            int pos = i * src.cols + j;
-            if(src_ptr[pos] != src_ptr[pos + src.cols] || src_ptr[pos] != src_ptr[pos + 1]) {
+
+    int pos = 0;
+    for(int i = 0 ; i < src.rows; ++i) {
+        for(int j = 0 ; j < src.cols ; ++j) {
+            pos = i * src.cols + j;
+
+            bool is_border = false;
+            if(i > 0)
+                is_border |= src_ptr[pos] != src_ptr[pos - src.cols];
+            if(i < src.rows - 1)
+                is_border |= src_ptr[pos] != src_ptr[pos + src.cols];
+            if(j > 0)
+                is_border |= src_ptr[pos] != src_ptr[pos - 1];
+            if(j < src.cols - 1)
+                is_border |= src_ptr[pos] != src_ptr[pos + 1];
+
+            if(is_border)
                 dst_ptr[pos] = 1;
-            }
         }
     }
 }
