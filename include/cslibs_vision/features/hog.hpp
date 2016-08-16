cbbc2922fd94b1d9a5a8312486b03917b217297e
Richard Hanten <richard.hanten@uni-tuebingen.de>
2016-08-16 23:19:43 +0200

    fixed hog

--- include/cslibs_vision/features/hog.hpp
+++ include/cslibs_vision/features/hog.hpp
@@ -24,15 +24,19 @@ public:
         if(src.channels() > 1)
             throw std::runtime_error("Input matrix needs to be single channel!");
 
-        Magnitude::compute(src, magnitude);
+        cv::Mat magnitude;
+        cv::Mat dx;
+        cv::Mat dy;
+        Magnitude::compute(src, magnitude, dx, dy);
 
-        dst_bins = cv::Mat(magnitude.rows, magnitude.cols, CV_8UC1, cv::Scalar());
-        dst_weights = cv::Mat(magnitude.rows, magnitude.cols, CV_32FC1, cv::Scalar());
 
         const std::size_t bins = M_PI / bin_size;
         const float *dx_ptr = dx.ptr<float>();
         const float *dy_ptr = dy.ptr<float>();
         const float *magnitude_ptr = magnitude.ptr<float>();
+
+        dst_bins = cv::Mat(magnitude.rows, magnitude.cols, CV_8UC1, cv::Scalar());
+        dst_weights = cv::Mat(magnitude.rows, magnitude.cols, CV_32FC1, cv::Scalar());
         uchar *dst_bins_ptr = dst_bins.ptr<uchar>();
         float *dst_weights_ptr = dst_weights.ptr<float>();
 
@@ -66,17 +70,20 @@ public:
         if(src.channels() > 1)
             throw std::runtime_error("Input matrix needs to be single channel!");
 
-        Magnitude::compute(src, magnitude);
+        cv::Mat dx;
+        cv::Mat dy;
+        Magnitude::compute(src, magnitude, dx, dy);
 
-        dst.resize(bins, cv::Mat(magnitude.rows, magnitude.cols, CV_32FC1, cv::Scalar()));
-        max_magnitude = std::numeric_limits<double>::lowest();
 
         const std::size_t bins = M_PI / bin_size;
         const float *dx_ptr = dx.ptr<float>();
         const float *dy_ptr = dy.ptr<float>();
         const float *magnitude_ptr = magnitude.ptr<float>();
+
+        dst.resize(bins, cv::Mat(magnitude.rows, magnitude.cols, CV_32FC1, cv::Scalar()));
+        max_magnitude = std::numeric_limits<double>::lowest();
         std::vector<float*> ptr_to_channels;
-        for(cv::Mat &channel : bins) {
+        for(cv::Mat &channel : dst) {
             ptr_to_channels.emplace_back(channel.ptr<float>());
         }
         float **channels_ptr = ptr_to_channels.data();
@@ -91,7 +98,7 @@ public:
             if(m > max_magnitude) {
                 max_magnitude = m;
             }
-            std::size_t index = angle / bin_size % bins;
+            std::size_t index = (std::size_t)(angle / bin_size) % bins;
             channels_ptr[index][i] = m; // * 255
         }
     }
@@ -113,7 +120,9 @@ public:
 
 
         cv::Mat magnitude;
-        Magnitude::compute(src, magnitude);
+        cv::Mat dx;
+        cv::Mat dy;
+        Magnitude::compute(src, magnitude, dx, dy);
 
         dst_bins = cv::Mat(magnitude.rows, magnitude.cols, CV_8UC1, cv::Scalar());
         dst_weights = cv::Mat(magnitude.rows, magnitude.cols, CV_32FC1, cv::Scalar());
@@ -128,7 +137,7 @@ public:
         const int size = magnitude.rows * magnitude.cols;
         for(int i = 0 ; i < size ; ++i) {
             double angle = atan2(dy_ptr[i], dx_ptr[i]) + M_PI;
-            std::size_t index = angle / bin_size % bins;
+            std::size_t index = (std::size_t)(angle / bin_size) % bins;
             dst_bins_ptr[i] = index;
             dst_weights_ptr[i] = magnitude_ptr[i]; // *  255
         }
@@ -153,13 +162,16 @@ public:
         if(src.channels() > 1)
             throw std::runtime_error("Input matrix needs to be single channel!");
 
+        cv::Mat dx;
+        cv::Mat dy;
         Magnitude::compute(src, magnitude);
-        dst.resize(channels, cv::Mat(magnitude.rows, magnitude.cols, CV_32FC1, cv::Scalar()));
 
         const std::size_t bins = 2 * M_PI / bin_size;
         const float *dx_ptr = dx.ptr<float>();
         const float *dy_ptr = dy.ptr<float>();
         const float *magnitude_ptr = magnitude.ptr<float>();
+
+        dst.resize(bins, cv::Mat(magnitude.rows, magnitude.cols, CV_32FC1, cv::Scalar()));
         std::vector<float*> ptr_to_channels;
         for(cv::Mat &channel : dst) {
             ptr_to_channels.emplace_back(channel.ptr<float>());
@@ -175,7 +187,7 @@ public:
                 max_magnitude = m;
             }
 
-            std::size_t index = angle / bin_size % bins;
+            std::size_t index = (std::size_t)(angle / bin_size) % bins;
             channels_ptr[index][i] = m; // * 255
         }
     }
